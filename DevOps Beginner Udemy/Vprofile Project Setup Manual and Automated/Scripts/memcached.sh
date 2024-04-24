18201bac3e21f340f3b1a590f633be5e7e47416f
Petar Djupanov <78720915+pdjupanov@users.noreply.github.com>
2024-04-24 04:32:02 +0300

    Create memcached.sh

---
+++ DevOps Beginner Udemy/Vprofile Project Setup Manual and Automated/Scripts/memcached.sh
@@ -0,0 +1,18 @@
+#!/bin/bash/
+
+dnf install epel-release -y
+dnf install memcached -y
+systemctl start memcached
+systemctl enable memcached
+systemctl status memcached
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/sysconfig/memcached
+systemctl restart memcached
+
+systemctl start firewalld
+systemctl enable firewalld
+
+firewall-cmd --add-port=11211/tcp
+firewall-cmd --runtime-to-permanent
+firewall-cmd --add-port=11111/udp
+firewall-cmd --runtime-to-permanent
+sudo memcached -p 11211 -U 11111 -u memcached -d
