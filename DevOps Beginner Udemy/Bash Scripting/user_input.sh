c5ec4e769e334f21a65e9cd3f37b6b7e06ffde32
Petar Djupanov <78720915+pdjupanov@users.noreply.github.com>
2024-06-08 17:08:31 +0300

    Create user_input.sh

---
+++ DevOps Beginner Udemy/Bash Scripting/user_input.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Enter your age: "
+read AGE
+
+echo "Your are $AGE years old."
+
+
+#login form
+
+read -p 'username: ' usr
+read -sp 'password: ' pass
+
+echo "######################"
+echo
+
+echo "Login Successfull: Welcome $usr,"
